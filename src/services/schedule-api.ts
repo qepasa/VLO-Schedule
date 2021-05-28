64468484b4c235b717162e8d511b3a8acd0d3a21
Cloud11665 <Cloud11665@gmail.com>
2021-05-28 12:03:45 +0200

    quick hack to get new api working

--- src/services/schedule-api.ts
+++ src/services/schedule-api.ts
@@ -3,11 +3,13 @@ import { GetScheduleResponse, GetClassesResponse } from "ApiModel";
 
 export function getSchedule(classParam: string, offset = 0): Promise<GetScheduleResponse> {
     console.log(`got class param ${classParam} offset: ${offset}`);
-    return fetch(`https://sabat.dev/api/tta?c=${classParam}&o=${offset}`)
-        .then(response => response.json());
+    return fetch(`https://api.cld.sh/vlo/ttdata/${classParam}?offset=${offset}`)
+        .then(response => response.json())
+        .then(function(a){return {success:true, resp:a}});
 }
-
+// TODO(cloud11665) fix this hackjob
 export function getClasses(): Promise<GetClassesResponse> {
-    return fetch(`https://sabat.dev/api/cla`)
-        .then(response => response.json());
+    return fetch(`https://api.cld.sh/vlo/listclass`)
+        .then(response => response.json())
+        .then(function(a){return {success:true, resp:a}});
 }
